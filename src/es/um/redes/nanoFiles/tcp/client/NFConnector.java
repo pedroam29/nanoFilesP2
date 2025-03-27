3bff1100cc553c00b208967e7fa10a0955c5122b
PedroRuizMendoza <pedroantonio.ruizm@um.es>
2025-03-27 10:35:29 +0100

    clase de practicas Boletin sockets TCP

--- src/es/um/redes/nanoFiles/tcp/client/NFConnector.java
+++ src/es/um/redes/nanoFiles/tcp/client/NFConnector.java
@@ -19,8 +19,8 @@ public class NFConnector {
 	private Socket socket;
 	private InetSocketAddress serverAddr;
 
-
-
+	DataInputStream dis  = null;
+	DataOutputStream dos = null;
 
 	public NFConnector(InetSocketAddress fserverAddr) throws UnknownHostException, IOException {
 		serverAddr = fserverAddr;
@@ -29,12 +29,16 @@ public class NFConnector {
 		 * servidor (IP, puerto). La creación exitosa del socket significa que la
 		 * conexión TCP ha sido establecida.
 		 */
+		socket = new Socket(serverAddr.getAddress(), serverAddr.getPort());
 		/*
 		 * TODO: (Boletín SocketsTCP) Se crean los DataInputStream/DataOutputStream a
 		 * partir de los streams de entrada/salida del socket creado. Se usarán para
 		 * enviar (dos) y recibir (dis) datos del servidor.
 		 */
-
+		dis = new DataInputStream(socket.getInputStream());
+		dos = new DataOutputStream(socket.getOutputStream());
+		
+		
 
 
 	}
@@ -44,6 +48,18 @@ public class NFConnector {
 		 * TODO: (Boletín SocketsTCP) Enviar entero cualquiera a través del socket y
 		 * después recibir otro entero, comprobando que se trata del mismo valor.
 		 */
+		
+		
+		try {
+			int integerTsend = 1;
+			int integerresived;
+			dos.writeInt(integerTsend);
+			integerresived = dis.readInt();
+			
+		}
+		catch (Exception e) {
+
+		}
 	}
 
 
